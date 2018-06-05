7060590296a37601a892b82c40fb054c3c7a98f8
Joao-Rocha <joaomdrocha@gmail.com>
2018-06-05 15:30:47 +0100

    first stuff

---
+++ js/app.js
@@ -0,0 +1,15 @@
+(function(){
+
+    var API_KEY = 'UbSoIrNYTGSqIPniW7WYOA';
+
+    /**
+     * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
+     * The below generateSignature should be done server side as not to expose your api secret in public
+     * You can find an eaxmple in PHP here: https://gist.github.com/joshuawoodward/7574df3df9a089e2663582a2cf9f188b
+     */
+    var API_SECRET = 'yXKuU56RnW8Du1VzJ7fY3UwulW35shTPk2V6';
+
+
+
+
+})();
