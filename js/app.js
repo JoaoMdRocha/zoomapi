25879636147989032fcf60e018da507f5898d976
JoaoMdRocha <33037339+JoaoMdRocha@users.noreply.github.com>
2018-06-06 09:17:31 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 (function(){
 
-    var API_KEY = 'UbSoIrNYTGSqIPniW7WYOA';
+    var API_KEY = 'INSERT API KEY HERE';
 
     /**
      * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
      * The below generateSignature should be done server side as not to expose your api secret in public
      * You can find an eaxmple in PHP here: https://gist.github.com/joshuawoodward/7574df3df9a089e2663582a2cf9f188b
      */
-    var API_SECRET = 'yXKuU56RnW8Du1VzJ7fY3UwulW35shTPk2V6';
+    var API_SECRET = 'INSERT API SECRET HERE';
 
 
     document.getElementById('requestPMI').addEventListener('click', function(e){
 
 
         var userId = document.getElementById('user_email').value;
-var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJVYlNvSXJOWVRHU3FJUG5pVzdXWU9BIiwiZXhwIjoxNDk2MDE5NjQwMDB9.2SjunNWo9ITW-4R_NnFkpGFXjPIAC7yLiJO_LoWArcM';
+var token = 'INSERT TOKEN HERE';
 
         $.ajax({
    type: "GET",
